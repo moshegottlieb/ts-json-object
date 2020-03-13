23df5ea6373acccbf32fc7efcb8a2160386a9900
Moshe Gottlieb <moshe@sharkfood.com>
2020-03-13 08:28:37 +0100

    tests can be async test formatting divided a few tests to several tests

--- test/index.ts
+++ test/index.ts
@@ -1,23 +1,45 @@
 import {JSONObject, required,optional, union, map, gt, lt , gte, lte, eq, ne, validate, passthrough, integer, array } from '../src/index'
 import {strict as assert} from 'assert'
 
-type TestFn = ()=>void
-type Tests = Array<TestFn >
+type TestFn = ()=>(void | Promise<void>)
+interface TestObject {
+    name:string
+    run:TestFn
+}
+type Tests = Array<TestObject>
+
+const CLEAR = "\x1b[0m"
+const BOLD = "\x1b[1m"
+const FAIL = "\x1b[31m"
+const OK = "\x1b[32m"
+const RUN = "\x1b[33m"
+
 
 class Test {
     constructor(tests:Tests){
         this._tests = tests
     }
-    run(){
-        const CLEAR = "\x1b[0m"
-        const BOLD = "\x1b[1m"
-        const FAIL = "\x1b[31m"
-        const OK = "\x1b[32m"
 
+    async run(){
+        
+        const COUNT = '' + this._tests.length
+        // Find max name length for preattier printing
+        let name_len = 0
         for (let i=0;i<this._tests.length;++i){
-            process.stdout.write(`[${i+1}/${this._tests.length}] - ${BOLD}`)
+            let test = this._tests[i]
+            name_len = Math.max(test.name.length,name_len)
+        }
+        for (let i=0;i<this._tests.length;++i){
+            let test = this._tests[i]
+            process.stdout.write(`[${(''+(i+1)).padStart(COUNT.length)}/${COUNT} ${RUN}${test.name.padEnd(name_len)}${CLEAR}] - ${BOLD}`)
             try {
-                this._tests[i]()
+                let ret = test.run()
+                if (ret instanceof Promise){
+                    let wait = async (promise:Promise<void>)=>{
+                        return promise
+                    }
+                    await wait(ret)
+                }
                 process.stdout.write(`${OK}OK`)
             } catch (error){
                 process.stdout.write(`${FAIL}FAILED`)
@@ -30,34 +52,58 @@ class Test {
 }
 
 let test = new Test([
-    ()=>{
+    {
+        name:'required',
+        run:()=>{
+            class Book extends JSONObject {
+                @JSONObject.required
+                name:string
+            }
+            const title = 'Moby Dick'
+            let book:Book = new Book({ name: title })
+            assert.ok(book.name == title)
+            assert.throws(()=>{
+                // Name is required
+                let book = new Book
+            })
+        }
+    },
+    {
+        name:'type',
+        run:()=>{
+            class Book extends JSONObject {
+                @JSONObject.required
+                name:string
+            }
+            const title = 'Moby Dick'
+            let book:Book = new Book({ name: title })
+            assert.ok(book.name == title)
+            assert.throws(()=>{
+                // Wrong type for name
+                let book = new Book({ name: 12345 } )
+            })
+        }
+    },
+    {
+        name:'optional',
+        run:()=>{
         class Book extends JSONObject {
-            @JSONObject.required
-            name:string
             @JSONObject.optional
-            summary?:string
+            name:string
         }
         const title = 'Moby Dick'
-        const summary = "You're my obsession"
-        let book:Book = new Book({ name: title , summary: summary })
-        assert.ok(book.name == title)
-        assert.ok(book.summary == summary)
-        let b2:Book = new Book({ name: title }) // Also ok
-        assert.ok(book.name == title)
-        assert.ok(b2.summary === undefined)
-        assert.throws(()=>{
-            let book = new Book({ name: 12345 } )
-        })
-        assert.throws(()=>{
-            let book = new Book({ summary: "Once upon a time" })
-        })
-    },
-    ()=>{        
+        let book1:Book = new Book({ name: title })
+        assert.ok(book1.name == title)
+        let book2:Book = new Book
+        assert.ok(book2.name == undefined)
+    }},
+    {
+        name:'recursive',
+        run:()=>{        
         class Author extends JSONObject {
             @JSONObject.required
             name: string
         }
-
         class BookWithAuthor extends JSONObject {
             @JSONObject.required
             name:string
@@ -72,17 +118,20 @@ let test = new Test([
         assert.ok(book.name == title)
         assert.ok(book.author.name == author)
         assert.ok(book.author instanceof Author)
-    },
-    ()=>{
+    }},
+    {
+        name:'unannonated',
+        run:()=>{
         class Person extends JSONObject {
             id:number
         }
         let p = new Person({id: 10})
         assert.ok(p.id === undefined)
-    },
-    ()=>{
+    }},
+    {
+        name:'lt-gt-ne',
+        run:()=>{
         class Person extends JSONObject {
-            @required
             @lt(10)
             @gt(5)
             @ne(6)
@@ -98,8 +147,11 @@ let test = new Test([
             let p = new Person({id: 6})
         })
         let p = new Person({id: 7})
-    },
-    ()=>{
+        assert.ok(p.id == 7)
+    }},
+    {
+        name:'lte',
+        run:()=>{
         class Person extends JSONObject {
             @lte(5)
             id:number
@@ -108,9 +160,13 @@ let test = new Test([
             let p = new Person({id: 6})
         })
         let p1 = new Person({id: 5})
+        assert.ok(p1.id == 5)
         let p2 = new Person({id: 4})
-    },
-    ()=>{
+        assert.ok(p2.id == 4)
+    }},
+    {
+        name:'gte',
+        run:()=>{
         class Person extends JSONObject {
             @gte(5)
             id:number
@@ -119,11 +175,14 @@ let test = new Test([
             let p = new Person({id: 4})
         })
         let p1 = new Person({id: 5})
+        assert.ok(p1.id == 5)
         let p2 = new Person({id: 6})
-    },
-    ()=>{
+        assert.ok(p2.id == 6)
+    }},
+    {
+        name:'eq',
+        run:()=>{
         class Person extends JSONObject {
-            @required
             @eq(10)
             id:number
         }
@@ -131,21 +190,26 @@ let test = new Test([
             let p = new Person({id: 11})
         })
         let p = new Person({id: 10})
-    },
-    ()=>{
+        assert.ok(p.id == 10)
+    }},
+    {
+        name:'validate',
+        run:()=>{
         class Person extends JSONObject {
-            @required
             @validate((person:Person,key:string,value:number)=>{
                 if (value < 8) throw new Error()
             })
             id:number
         }
         let p = new Person({id : 10})
+        assert.ok(p.id == 10)
         assert.throws(()=>{
             let p = new Person({ id: 7 })
         })
-    },
-    ()=>{
+    }},
+    {
+        name:'union',
+        run:()=>{
         class Person extends JSONObject {
             @required
             @union([1,2,3,4,5])
@@ -153,25 +217,35 @@ let test = new Test([
         }
         for (let i=1;i<=5;++i){
             let p = new Person({id : i})
+            assert.ok(p.id == i)
         }
         assert.throws(()=>{
             let p = new Person({ id: 6 })
         })
-    },
-    ()=>{
+    }},
+    {
+        name:'map',
+        run:()=>{
         const title = 'Moby Dick'
         class Book extends JSONObject {
             @map('title')
-            @required
             name:string
         }
         let book = new Book({ title: title })
         assert.ok(book.name == title)
         assert.throws(()=>{
+            // Make 'name' required, make sure this fails
+            class Book extends JSONObject {
+                @map('title')
+                @required
+                name:string
+            }
             let book = new Book({ name: title })
         })
-    },
-    ()=>{
+    }},
+    {
+        name:'passthrough',
+        run:()=>{
         class Car extends JSONObject {
             @passthrough
             userManual:any
@@ -183,9 +257,12 @@ let test = new Test([
         let car3 = new Car({ userManual : {
             pages : ['page1','page2']
         }})
+        assert.ok(car3.userManual.pages[0] == 'page1')
         assert.ok(car3.userManual.pages[1] == 'page2')
-    },
-    ()=>{
+    }},
+    {
+        name:'default',
+        run:()=>{
         class Car extends JSONObject {
             @optional(false)
             electric:boolean
@@ -194,10 +271,11 @@ let test = new Test([
         assert.ok(electric_car.electric)
         let carbon_emitting_car = new Car({})
         assert.ok(carbon_emitting_car.electric === false)
-    },
-    ()=>{
+    }},
+    {
+        name:'integer',
+        run:()=>{
         class Int extends JSONObject {
-            @required
             @integer
             value:number
         }
@@ -205,14 +283,33 @@ let test = new Test([
         assert.throws(()=>{
             let i = new Int({ value: 13.4 })
         })
-    },
-    ()=>{
+    }},
+    {
+        name:'array_simple',
+        run:()=>{
+        class SimpleArrayTest extends JSONObject {
+            @array(String)
+            strings:[string]
+            @array(Number)
+            numbers:[number]
+        }
+        let sa1 = new SimpleArrayTest({ strings: ['a','b','c']})
+        assert.throws(()=>{
+            let sa2 = new SimpleArrayTest({ strings: [1,2,3]})
+        })
+        let sa2 = new SimpleArrayTest({ numbers: [1,2,3]})
+        assert.throws(()=>{
+            let sa2 = new SimpleArrayTest({ numbers: ['a','b','c']})
+        })
+    }},
+    {
+        name:'array_object',
+        run:()=>{
         class Element extends JSONObject {
             @optional
             x?:number
         }
         class ArrayTest extends JSONObject {
-            @required
             @array(Element)
             a:Array<Element>
         }
@@ -222,8 +319,10 @@ let test = new Test([
         assert.ok(arrayTest.a[0].x == 2)
         assert.ok(arrayTest.a[1].x === undefined)
         assert.ok(arrayTest.a.length == 2)
-    },
-    ()=>{
+    }},
+    {
+        name:'array_passthrough',
+        run:()=>{
         // Simply type
         class ArrayTest extends JSONObject {
             @required
@@ -234,10 +333,6 @@ let test = new Test([
         assert.ok(arrayTest.a[0].x == 2)
         assert.ok(arrayTest.a[1].x === undefined)
         assert.ok(arrayTest.a.length == 2)
-    }
+    }}
 ])
 test.run()
-
-// Summary is undefined
-// This will throw a TypeError, as 'name' is required
-
